408d3c6cb82bcdb61ba0b0e146092974320f76f9
LUNKUS <sigitas@lunkus.com>
2020-04-24 08:32:41 +0300

    Main frame done

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame main = new JFrame("GrannDB Client");
+
+        main.setSize(800,600);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // When close the window close the application
+        main.setVisible(true);
     }
 }

39858ed1b93baf4cb3586a2be646bd2a21b6360f
LUNKUS <sigitas@lunkus.com>
2020-04-26 09:55:50 +0300

    connector dialog

--- src/ConnetorDialog.java
+++ src/ConnetorDialog.java
@@ -1,2 +1,16 @@
-public class ConnetorDialog {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Properties;
+
+public class ConnetorDialog extends JDialog implements ActionListener {
+
+    public ConnetorDialog(JFrame owner, String title, Properties p) {
+        super(owner,title,true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }

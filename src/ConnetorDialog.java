893fc8db4f8ba392d62c4bff933380c04324c6b4
LUNKUS <sigitas@lunkus.com>
2020-04-27 07:29:03 +0300

    buttons and labels created

--- src/ConnetorDialog.java
+++ src/ConnetorDialog.java
@@ -5,10 +5,32 @@ import java.util.Properties;
 
 public class ConnetorDialog extends JDialog implements ActionListener {
 
-    public ConnetorDialog(JFrame owner, String title, Properties p) {
+    Properties dbProperties;
+
+    JLabel hostLBL = new JLabel("Host");
+    JTextField host = new JTextField();
+
+    JLabel portLBL = new JLabel("Port");
+    JTextField port = new JTextField();
+
+    JLabel dbNameLBL = new JLabel("DB Name");
+    JTextField dbName = new JTextField();
+
+    JLabel userLBL = new JLabel("User");
+    JTextField user = new JTextField();
+
+    JLabel passLBL = new JLabel("Pasword");
+    JTextField pass = new JTextField();
+
+    JButton submit = new JButton("submit");
+    JButton cancel = new JButton("cancel");
+
+    public ConnetorDialog(JFrame owner, String title, Properties props) {
         super(owner,title,false);
 
         setSize(400,400);
+
+        dbProperties = props;
     }
 
     @Override

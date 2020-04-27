6c95a4583df615adcc4c398259f6ecdf4b40f36b
LUNKUS <sigitas@lunkus.com>
2020-04-27 07:40:58 +0300

    addede buttons and labels to the data & button panels

--- src/ConnetorDialog.java
+++ src/ConnetorDialog.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Properties;
@@ -22,15 +23,37 @@ public class ConnetorDialog extends JDialog implements ActionListener {
     JLabel passLBL = new JLabel("Pasword");
     JTextField pass = new JTextField();
 
-    JButton submit = new JButton("submit");
-    JButton cancel = new JButton("cancel");
+    JButton submitBTN = new JButton("submit");
+    JButton cancelBTN = new JButton("cancel");
 
     public ConnetorDialog(JFrame owner, String title, Properties props) {
-        super(owner,title,false);
+        super(owner,title,true);
 
         setSize(400,400);
 
         dbProperties = props;
+
+        JPanel dataPanel = new JPanel();
+
+        dataPanel.setLayout(new GridLayout(5, 2));
+
+        dataPanel.add(hostLBL);
+        dataPanel.add(host);
+        dataPanel.add(portLBL);
+        dataPanel.add(port);
+        dataPanel.add(dbNameLBL);
+        dataPanel.add(dbName);
+        dataPanel.add(userLBL);
+        dataPanel.add(user);
+        dataPanel.add(passLBL);
+        dataPanel.add(pass);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(submitBTN);
+        buttonPanel.add(cancelBTN);
+
+        add(dataPanel, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.SOUTH);
     }
 
     @Override
